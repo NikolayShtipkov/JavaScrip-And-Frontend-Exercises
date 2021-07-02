071c1fd8af66ad411d7bc43cdd0cfef2e15d3f0d
NikolayShtipkov <nikolayshtipkov@gmail.com>
2021-07-02 20:35:41 +0300

    Practising Babel

---
+++ Using-Babel/lib/src/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var person = 'Giorno Giovanna';
+
+var stand = 'Gold Expiriance';
+
+var theme = 'vento de oro';
+var cry = 'MudaMudaMudaMuda.... MUDA!!';
+
+console.log('I ' + person + ' have a dream');
---
+++ Using-Babel/src/src/main.js
@@ -0,0 +1,8 @@
+const person = 'Giorno Giovanna';
+
+let stand = 'Gold Expiriance';
+
+var theme = 'vento de oro';
+var cry = 'MudaMudaMudaMuda.... MUDA!!';
+
+console.log(`I ${person} have a dream`);
